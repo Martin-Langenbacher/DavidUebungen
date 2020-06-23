efdd296c549afb6aff683e0b8f4ea840b264c2ca
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-06-23 16:33:08 +0200

    first commit

---
+++ wcs-java-transactions-quest-master/wcs-java-tx-jdbc-quest/src/main/java/com/wcs/java/tx/jdbc/Main.java
@@ -0,0 +1,42 @@
+package com.wcs.java.tx.jdbc;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		// 3 Konten anlegen:
+		
+		
+		
+		Connection con = DriverManager
+				.getConnection("jdbc:mysql://my_stuff:my_stuff@localhost:3306/my_stuff?serverTimezone=CET");
+		
+		//SetupUtil.setup();
+		
+		try {
+			
+		con.setAutoCommit(false);
+		AccountService accountService = new AccountService(con);
+		TransferService transferService = new TransferService(accountService, con );
+		transferService.transferMoney("andre", "david", new BigDecimal(500));
+//		Statement st = con.createStatement();
+//		st.execute("UPDATE bankaccounts SET balance=1000 WHERE user='david'");
+//		st.execute("UPDATE bankaccounts SET balance=2000 WHERE user='andre'");
+		con.commit(); 
+		// erst nach dem commit sind die Daten in der Datenbank!
+		} catch(Exception ex) {
+			con.rollback();
+		}finally {
+			con.close();
+		}
+
+	}
+
+}
---
+++ wcs-java-transactions-quest-master/wcs-java-tx-jdbc-quest/src/main/java/com/wcs/java/tx/jdbc/TransferService.java
@@ -0,0 +1,43 @@
+package com.wcs.java.tx.jdbc;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TransferService {
+	
+	// so hole ich die Möglichkeiten von AccountService in TransferService !!!!!!!!!!!!!!!!!!!!!!!
+	
+	// könnte man auch instanz-Variable, 
+	private AccountService accountService;
+	private Connection con;
+	
+	// Constructor anlegen, der den AccountService anlegt & zusätzlich die Connection mit übergeben...
+	public TransferService(AccountService accountService, Connection con) {
+		super();
+		this.accountService = accountService;
+		this.con = con;
+	}
+	
+	
+
+	public void transferMoney(String userFrom, String userTo, BigDecimal amount)
+			throws SQLException, InsufficientFundsException {
+		
+		try {
+			// ausgeblendet klappt es nicht: Zeile 29, 32, 35
+			con.setAutoCommit(false);
+			// Reihenfolge spielt eine Rolle, da er sonst vorher abbricht und in den catch-Block springt.
+			accountService.deposit(userTo, amount);
+			accountService.withdraw(userFrom, amount);
+			con.commit(); 
+	
+			} catch(InsufficientFundsException ex) {
+				con.rollback();
+				throw ex;
+			}
+		
+		
+	}
+
+}
